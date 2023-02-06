98d87db8c5ccb98217eef386492ab1f778c7fcdb
gravilaxe <joshuawright3132@outlook.com>
2023-02-06 16:30:38 +0000

    Initial Commit

---
+++ LatinBooks/LatinBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LatinBooksApp.swift
+//  LatinBooks
+//
+//  Created by Joshua Wright on 06/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct LatinBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
